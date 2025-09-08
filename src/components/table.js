6965a992c58fec00a9063e411c5dd916978902af
Ivan Titov <vanyat296@gmail.com>
2025-09-08 15:57:33 +0300

    текст-сортировка

--- src/components/table.js
+++ src/components/table.js
@@ -29,7 +29,7 @@ export function initTable(settings, onAction) {
     root.container.addEventListener('submit', (e) => {
         e.preventDefault();
         e.submitter.setAttribute('name', 'clear');
-
+        e.submitter.setAttribute('name', 'sort');
         onAction(e.submitter);
     });
 
